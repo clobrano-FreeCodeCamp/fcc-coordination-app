1af1a2b50a79ed2ee7fafb5740db1f37e6c2cab0
Carlo Lobrano <c.lobrano@gmail.com>
2017-06-23 22:59:22 +0200

    yelp: added caching of search results

--- yelp.js
+++ yelp.js
@@ -1,11 +1,17 @@
 'use strict';
-
 const yelp = require('yelp-fusion');
+const NodeCache = require ('node-cache');
 
 const clientId = process.env.YELP_ID
 const clientSecret = process.env.YELP_SECRET
+const cache = new NodeCache();
 
 module.exports.get_places = function (location, callback) {
+  const cached = cache.get (location);
+
+  if (cached)
+    return callback (null, cached);
+
   yelp.accessToken(clientId, clientSecret).then(response => {
     const client = yelp.client(response.jsonBody.access_token);
 
@@ -13,7 +19,9 @@ module.exports.get_places = function (location, callback) {
       term:'restaurant pizza',
       location: location
     }).then(response => {
-      callback (null, response.jsonBody.businesses);
+      const places = response.jsonBody.businesses;
+      cache.set (location, places);
+      callback (null, places);
     });
   }).catch(e => {
     console.log(e);
